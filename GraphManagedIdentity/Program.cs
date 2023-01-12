97a478d18953496a7a38d9137b27d864e6b806ba
damienbod <damien_bod@hotmail.com>
2023-01-12 14:08:47 +0100

    demo app, always show exceptions

--- GraphManagedIdentity/Program.cs
+++ GraphManagedIdentity/Program.cs
@@ -6,20 +6,11 @@ namespace GraphManagedIdentity
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddRazorPages();
 
             builder.Services.AddScoped<AadGraphSdkManagedIdentityAppClient>();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
